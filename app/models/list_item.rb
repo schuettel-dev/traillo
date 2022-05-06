712f6cebed6ff2a2073a9298cb5ecaff9c402b8f
mario <github@lxxxvi.ch>
2022-05-06 17:01:26 +0200

    add stream and broadcast hook

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -5,4 +5,19 @@ class ListItem < ApplicationRecord
   validates :list_position, numericality: { only_integer: true, greater_than: 0 }
 
   scope :ordered, -> { order(list_position: :asc) }
+
+  after_commit :broadcast_list_items
+
+  private
+
+  def broadcast_list_items
+    broadcast_replace_to(
+      :funky_list_items,
+      target: "list_items",
+      partial: "list_items/list_items",
+      locals: {
+        list_items: ListItem.ordered
+      }
+    )
+  end
 end
