17137001e5a4aae1f2226e2b14cbe0aeb7e045ef
mario <github@lxxxvi.ch>
2022-05-06 13:53:07 +0200

    add validations to `ListItem`

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,3 +1,6 @@
 class ListItem < ApplicationRecord
   has_rich_text :description
+
+  validates :title, presence: true
+  validates :list_position, numericality: { only_integer: true, greater_than: 0 }
 end
