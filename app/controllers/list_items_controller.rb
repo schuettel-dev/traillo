d7169aeb9623f4fd5afb8ce459a5bc0530173070
mario <github@lxxxvi.ch>
2022-05-06 09:57:17 +0200

    redirect to list_items_path after create

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -24,7 +24,7 @@ class ListItemsController < ApplicationController
     @list_item = ListItem.new(list_item_params)
 
     if @list_item.save
-      redirect_to @list_item, notice: "List item was successfully created."
+      redirect_to list_items_path, notice: "List item was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
